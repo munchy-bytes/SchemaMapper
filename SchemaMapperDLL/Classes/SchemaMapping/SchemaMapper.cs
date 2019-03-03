1d5d5ec803f6dd341ed2bfea9a9dc52135a5f0c2
Hadi <hadi.fadlullah@gmail.com>
2019-03-04 00:42:03 +0200

    SchemaMapper_Column class moveed to a seperate file

--- SchemaMapperDLL/Classes/SchemaMapping/SchemaMapper.cs
+++ SchemaMapperDLL/Classes/SchemaMapping/SchemaMapper.cs
@@ -586,44 +586,5 @@ namespace SchemaMapperDLL.Classes.SchemaMapping
         }
 
     }
-    public class SchemaMapper_Column
-    {
-        #region properties
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string DefaultValue { get; set; }
-        public bool IsExpression { get; set; } = false;
-        public string Expression { get; set; }
-        public ColumnDataType DataType { get; set; }
-        public List<string> MappedColumns { get; set; }
-        
-        #endregion
-
-        #region methods
-        public SchemaMapper_Column(int id, string name, string defValue, bool isexpression, string expression, ColumnDataType datatype)
-        {
-
-            ID = id;
-            Name = name;
-            DefaultValue = defValue;
-            IsExpression = isexpression;
-            Expression = expression;
-            DataType = datatype;
-            MappedColumns = new List<string>();
-
-
-        }
-        #endregion
-
-        #region declarations
-        public enum ColumnDataType
-        {
-            Text = 0,
-            Memo = 1,
-            Number = 2,
-            Date = 3
-        }
-        #endregion
-    }
 
 }
