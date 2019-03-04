8a70e9e4ba53f5dc1685d22430ec860e168783e9
Hadi <hadi.fadlullah@gmail.com>
2019-03-04 23:56:47 +0200

    Added assembly informations

--- SchemaMapperDLL/Properties/AssemblyInfo.cs
+++ SchemaMapperDLL/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SchemaMapperDLL")]
+[assembly: AssemblyTitle("SchemaMapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SchemaMapperDLL")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("SchemaMapper")]
+[assembly: AssemblyCopyright("Copyright © Munchy Bytes 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
